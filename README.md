d5a386420f70f2cdff8c151300062c8f55104907
YoEight <yo.eight@gmail.com>
2021-05-30 20:42:03 +0200

    Switch <leader> key to <space>.

--- README.md
+++ README.md
@@ -7,6 +7,11 @@ expected to be located in `$HOME/.config/nvim`
 ## Requirements
 * [packer.nvim] A Neovim package manager.
 * [rust-analyzer] is expected to be in `$PATH`
+* [ripgrep]
+
+## Personal notes
+* If LSP doesn't seem to work, you probably didn't start NeoVim at the root of your project.
 
 [packer.nvim]: https://github.com/wbthomason/packer.nvim#quickstart
 [rust-analyzer]: https://rust-analyzer.github.io/manual.html#installation
+[ripgrep]: https://github.com/BurntSushi/ripgrep#installation
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,3 +1,5 @@
+vim.g.mapleader = ' '
+
 local common = require 'common'
 
 -- Telescope
@@ -7,8 +9,19 @@ common.map('n', '<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<c
 common.map('n', '<leader>fh', "<cmd>lua require('telescope.builtin').help_tags()<cr>")
 
 -- Nvim Tree
-common.map('n', '<C-n>', ':NvimTreeToggle<cr>')
+-- common.map('n', '<C-n>', ':NvimTreeToggle<cr>')
 common.map('n', '<leader>r', ':NvimTreeRefresh<cr>')
-common.map('n', '<leader>n', ':NvimTreeFindFile<cr>')
+common.map('n', '<leader>n', ':NvimTreeToggle<cr>')
+
+-- Windows
+common.map('n', '<leader>wv', ':vsplit<cr>')
+common.map('n', '<leader>ws', ':split<cr>')
+common.map('n', '<leader>wk', ':wincmd k<cr>')
+common.map('n', '<leader>wj', ':wincmd j<cr>')
+common.map('n', '<leader>wh', ':wincmd h<cr>')
+common.map('n', '<leader>wl', ':wincmd l<cr>')
+
+-- Misc
+common.map('n', '<leader>q', ':q<cr>')
 
 vim.cmd [[highlight NvimTreeFolderIcon guibg=blue]]
