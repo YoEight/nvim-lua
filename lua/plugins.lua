b7865d1642598130014bc136ae3beb01dd2e1a4e
YoEight <yo.eight@gmail.com>
2024-12-27 23:15:07 -0500

    Implement dark/light mode detection.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -131,4 +131,19 @@ return {
     dependencies = { "github/copilot.vim"},
     build = "make tiktoken",
   },
+
+  {
+    "f-person/auto-dark-mode.nvim",
+    opts = {
+      update_interval = 1000,
+      set_dark_mode = function()
+        vim.api.nvim_set_option_value("background", "dark", {})
+        vim.cmd("colorscheme catppuccin-mocha")
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option_value("background", "light", {})
+        vim.cmd("colorscheme catppuccin-latte")
+      end,
+    },
+  },
 }
